09858c167efde7c6a7e4d34ace792126089161dc
James Winkle <jjwinklejmj@gmail.com>
2022-05-01 14:54:01 -0500

    added ApiProperty decorator for swagger rendering

--- src/child/dto/create-child.dto.ts
+++ src/child/dto/create-child.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateChildDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   public name: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   public factoryId: number;
 }
--- src/factory/dto/create-factory.dto.ts
+++ src/factory/dto/create-factory.dto.ts
@@ -1,11 +1,16 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 // TODO: no extra junk in request
 export class CreateFactoryDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   public name: string;
 
+  @ApiProperty()
   public lowerBoundChildNodes: number;
+
+  @ApiProperty()
   public upperBoundChildNodes: number;
 }
